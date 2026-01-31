9e42ee6a8be4fc65cd40c27308d6a401268baaf5
Abel-Carpentier <abel.carpentier64@gmail.com>
2026-01-31 17:25:10 +0100

    Va te faire enculer Joan bztmr

--- src/main.py
+++ src/main.py
@@ -1,57 +1,139 @@
+import os
+import random
+import math
 import pygame
-import entities.player as pl
-
-# creation of the game window
-
-screen_width = 640
-screen_height = 480
-screen = pygame.display.set_mode((screen_width,screen_height))
-pygame.display.set_caption("Ecohead")
-
-# creation of the player
-
-player = pl.Player(100, 1,200, 200, 50)
-projectile = None
-
-# main
-
+from os import listdir
+from os.path import isfile, join
 pygame.init()
 
-playing = True
-while playing:
+pygame.display.set_caption("Platformer")
 
-    screen.fill((0,0,0))
-    pygame.draw.rect(screen,(0,255,0),pygame.Rect(player.posx,player.posy,player.size,player.size))
+WIDTH, HEIGHT = 1000, 800
+FPS = 60
+PLAYER_VEL = 5
 
-    for events in pygame.event.get():
-        if events.type == pygame.QUIT:
-            running = False
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 
-    key = pygame.key.get_pressed()
-    if key[pygame.K_q] and player.posx > 0:
-        player.moveLeft()
-    if key[pygame.K_d] and player.posx < screen_width-50:
-        player.moveRight()
-    if key[pygame.K_SPACE] and player.posy >= screen_height-50:
-        player.jump()
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
 
-    projectiles = []
-    if key[pygame.K_z]:
-        projectile = player.tirer()
-        projectiles.append(projectile)
+def load_sprite_sheets(dir1, dir2, width, height, direction = False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))] #lire tous les fichiers dans un dossier jcrois
 
-    compteur = 0
-    for projectile in projectiles[:] :
-        if projectile is not None:
-            projectile.avancer()
-            pygame.draw.rect(screen, (255, 255, 0), pygame.Rect(projectile.posx, projectile.posy, 10, 4))
-            compteur += 1
-            print(compteur)
-            if projectile.get_posx() > screen_width or projectile.get_posx() < 0 :
-                projectiles.remove(projectile)
-                print("dell")
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect) #blit = draw
+            sprites.append(pygame.transform.scale2x(surface))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+
+    return all_sprites
+
+
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+    GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
+
+    def __init__(self, x, y, widht, height):
+        self.rect = pygame.Rect(x, y, widht, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+        self.fall_count = 0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+
+    def loop(self, fps):
+        #self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+        self.move(self.x_vel, self.y_vel)
+
+        self.fall_count += 1
+
+    def draw(self, win):
+        self.sprite = self.SPRITES["idle_" + self.direction][0]
+        win.blit(self.sprite, (self.rect.x, self.rect.y))
+
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+
+def draw(window, background, bg_image, player):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    player.draw(window)
 
     pygame.display.update()
 
-    if player.posy<screen_height-50:
-        player.posy += 0.1
+
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+
+    player.x_vel = 0
+    if keys[pygame.K_q]:
+        player.move_left(PLAYER_VEL)
+    if keys[pygame.K_d]:
+        player.move_right(PLAYER_VEL)
+
+def main(window):
+    clock = pygame.time.Clock()
+    background, bg_image = get_background("Blue.png") #pour changer le background, juste changez la couleur. Par exemple écrivez Yellow.png
+
+    player = Player(100, 100, 50, 50)
+
+    run = True
+    while run:
+        clock.tick(FPS) #comme ça on est sur que ça tourne en 60fps
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        player.loop(FPS)
+        handle_move(player)
+        draw(window, background, bg_image, player)
+
+    pygame.quit()
+    quit()
+
+if __name__ == "__main__":
+    main(window)
